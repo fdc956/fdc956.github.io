59f1364ea7027f3214222c620c735c9fcaf4bb42
fdc956 <heartbeat9@riseup.net>
2019-04-30 00:24:33 -0700

    clean up index

---
+++ dj.js
@@ -0,0 +1,16 @@
+    $(document).ready(function(){
+        function getData(){
+            $.ajax({
+                type: 'POST',
+                url: 'dj.php',
+                success: function(data){
+                    $('#dj').html(data);
+                }
+            });
+        }
+        getData();
+        setInterval(function () {
+            getData();
+        }, 5000);  // it will refresh your data every 1 sec
+
+    });
